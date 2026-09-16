660dfcaafdb2e2043a2597f291b01e5274ffa7e1
murilo castro <murilocastro233@gmail.com>
2026-09-15 22:35:31 -0300

    aula api

--- src/pages/ProdutoPage.jsx
+++ src/pages/ProdutoPage.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react"
 
 
 const ProdutoPage = () => {
+  const API_URL = import.meta.env.VITE_API_URL  
+  const [products, setProducts] = useState([])
+  
+  useEffect(() => {
+    fetch(`${API_URL}products/category/electronics`)
+      .then((results) => results.json())
+      .then((dados) => setProducts(dados))
+  }, [])
+
   return (
-        
-    <div>
-        <h1>Página de Produto</h1>
-    </div>
+    <section>
+      <h1>Página de Produto</h1>
+      {products.map((produtoAtual) => (
+          <div>
+              <img src={produtoAtual.image} alt="" />
+              <h2>{produtoAtual.title}</h2>
+              <p>{produtoAtual.price}</p>
+          </div>
+      ))}
+    </section>      
+
   )
 }
 
