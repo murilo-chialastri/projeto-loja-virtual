4f7cd21fbc7e0716c600179c792d2988696f77ce
Murilo Castro <murilocastro233@gmail.com>
2026-09-23 15:04:28 -0300

    arrumei mais ou menos

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,13 @@
-function ProductCard([id, image, title, category, rating, price]) {
+function ProductCard({id, image, title, category, rating, price,onAddToCart}) {
     return (
         <div className = "product-card" key={id}>
                     <img src={image} alt={title} />
                     <p className="product-category">{category}</p>
                     <h3>{title}</h3>
-                    <p className="product-rating">⭐⭐⭐⭐☆ ({rating.rate})</p>
+                    <p className="product-rating">⭐⭐⭐⭐☆ ({rating})</p>
                     <p className="product-price">R$ {price}</p>
-                    <button className="btn-secondary" onClick={addToCart}>Adicionar ao carrinho</button>
-                </div >
+                    <button className="btn-secondary" onClick={onAddToCart}>Adicionar ao carrinho</button>
+        </div >
     )
 }
 
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -13,15 +13,19 @@ const Products = ({ setCart }) => {
         <section id="produtos" className="products">
             <h2>Produtos em destaque</h2>
         
-        <ProductCard
+        
+            <div className="product-list">
+
+            <ProductCard
                 id="1"
                 image="https://placehold.co/240x240/aa3bff/ffffff?text=Tenis"
                 title="Tênis esportivo"
                 category="Calçados"
                 rating="4.5"
                 price="299,90"
+                onAddToCart={addToCart}
         />
-            <div className="product-list">
+
                 <div className="product-card">
                     <img src="https://placehold.co/240x240/aa3bff/ffffff?text=Tenis" alt="Tênis esportivo" />
                     <p className="product-category">Calçados</p>
